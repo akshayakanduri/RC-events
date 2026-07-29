9b23773ecf627d318fb3ccfe84d0840832b22e99
akshayakanduri <akshayakanduri@gmail.com>
2026-07-29 20:23:56 +0530

    Switch email service to Brevo SMTP

--- event_management/server/utils/email.js
+++ event_management/server/utils/email.js
@@ -5,16 +5,13 @@ dotenv.config();
 
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: "smtp-relay.brevo.com",
     port: 587,
     secure: false,
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-    tls: {
-        rejectUnauthorized: false,
-    },
+        pass: process.env.EMAIL_PASS
+    }
 });
 
 transporter.verify(function (error, success) {
